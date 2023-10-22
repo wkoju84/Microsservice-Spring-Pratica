d4ce295620439226b3116ef255fdba80a5cef64f
wkoju84 <wkoju84@gmail.com>
2023-10-22 16:04:14 -0300

    Finalizando publish message

--- user/src/main/java/com/ms/user/services/UserService.java
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ms.user.services;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
 import com.ms.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,8 +13,15 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    UserProducer userProducer;
+
     @Transactional
     public UserModel save(UserModel userModel){
-        return userRepository.save(userModel);
+
+        userModel = userRepository.save(userModel); // Vai salvar no BD
+        userProducer.publishMessageEmail(userModel); // acionar o producer
+
+        return userModel; // retornar pro cliente
     }
 }
