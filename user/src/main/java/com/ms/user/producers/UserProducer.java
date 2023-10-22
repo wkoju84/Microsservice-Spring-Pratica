2b41005914dfcfa5680ee0242350dbd8272e924e
wkoju84 <wkoju84@gmail.com>
2023-10-22 15:56:58 -0300

    Adicionando producer e configurando EmailProducer

---
+++ user/src/main/java/com/ms/user/producers/UserProducer.java
@@ -0,0 +1,28 @@
+package com.ms.user.producers;
+
+import com.ms.user.dtos.EmailDto;
+import com.ms.user.models.UserModel;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserProducer {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @Value(value = "${broker.queue.email.name}")// Exchange do tipo default: chave vazia
+    private String routingKey;
+
+    public void publishMessageEmail(UserModel userModel) {
+        var emailDto = new EmailDto();
+        emailDto.setUserId(userModel.getUserId());
+        emailDto.setEmailTo(userModel.getEmail());
+        emailDto.setSubject("Cadastro realizado com sucesso!");
+        emailDto.setText(userModel.getName() + ", seja bem vindo(a)! \nAgradecemos o seu cadastro, aproveite agora todos os recursos da nossa plataforma!");
+
+        rabbitTemplate.convertAndSend("", routingKey, emailDto);
+    }
+}
